0364f18bff3310e3f1298bdc5a22e88fe7bf6869
Danielrib1989 <daniel.perisse.ribeiro@gmail.com>
2021-02-12 10:49:49 -0300

    edit page and fixed bug

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,8 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
 
+  validates :name, :ticker, presence: true
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_publishable_key],
                                   secret_token: Rails.application.credentials.iex_client[:sandbox_secret_key],
@@ -16,5 +18,5 @@ class Stock < ApplicationRecord
 
   def self.check_db(ticker_symbol)
     where(ticker: ticker_symbol).first
-  end    
+  end
 end
